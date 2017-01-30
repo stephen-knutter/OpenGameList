dbb0dbb44120c2c2c7d467ca1ee6079b1b7e98a9
Stephen Knutter <stephen.knutter@gmail.com>
2017-01-30 15:33:14 -0700

    add view & edit item components

--- OpenGameList/Scripts/app/item-list.component.ts
+++ OpenGameList/Scripts/app/item-list.component.ts
@@ -71,6 +71,6 @@ export class ItemListComponent implements OnInit {
     onSelect(item: Item) {
         this.selectedItem = item;
         console.log("Item " + this.selectedItem.Id + " has been clicked: loading ItemDetailComponent...");
-        this.router.navigate(["item", this.selectedItem.Id]);
+        this.router.navigate(["item/view", this.selectedItem.Id]);
     }
 }
